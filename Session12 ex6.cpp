884162a48dc771bf23cdfb5a554dc1fa1b21d773
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2024-12-10 15:49:28 +0700

    Add files via upload

---
+++ Session12 ex6.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int is_perfect(int number) {
+    int sum = 0;
+    for (int i = 1; i <= number / 2; i++) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+    return sum == number;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Nhap so nguyen thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so nguyen thu hai: ");
+    scanf("%d", &num2);
+
+    if (is_perfect(num1)) {
+        printf("%d la so hoan hao.\n", num1);
+    } else {
+        printf("%d khong phai la so hoan hao.\n", num1);
+    }
+
+    if (is_perfect(num2)) {
+        printf("%d la so hoan hao.\n", num2);
+    } else {
+        printf("%d khong phai la so hoan hao.\n", num2);
+    }
+
+    return 0;
+}
